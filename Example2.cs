141b353ce3c95ef08424a860e8c9a9deef6e5493
dr1mer.vm <dr1mer.vm@gmail.com>
2022-03-28 00:45:32 +0500

    add Example 2

---
+++ Example2.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Example2 {
+  static void Main() {
+    int x;
+    int y;
+    
+    x = 100;
+    
+    Console.WriteLine("x содержит " + x);
+    
+    y = x / 2;
+    
+    Console.WriteLine("y содержит x / 2: ");
+    Console.WriteLine(y);
+  }
+}

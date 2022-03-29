ede4785f264e9dd5671869a89294b1e1e52cd636
dr1mer.vm <dr1mer.vm@gmail.com>
2022-03-29 21:53:08 +0500

    add comments in code

--- Practice1.cs
+++ Practice1.cs
@@ -1,38 +1,38 @@
-using System;
+using System;   // Namespace Usage
 
-class Practice1 {
-  static void Main() {
+class Practice1 {   // Class declaration
+  static void Main() {  // Metod declaration
     
-    int result;
+    int result;   // Variable declaration
     
-    Console.Write("Введите число в десятичной системе: ");
-    int numb = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число в десятичной системе: ");  // Message output
+    int numb = Convert.ToInt32(Console.ReadLine());   // Variable declaration and type conversion
     
-    Console.Write("Введите основание: ");
-    int bas = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите основание: ");   // Message output
+    int bas = Convert.ToInt32(Console.ReadLine());  // Variable declaration and type conversion
     
-    if (bas < 2)
+    if (bas < 2)  // Declaring the first condition
     {
-    Console.WriteLine("Введите число от 2 до 20");
-    return;
+    Console.WriteLine("Введите число от 2 до 20");  // Message output
+    return;   // Cycle completion
     }
-    else if (bas > 20)
+    else if (bas > 20)  // Declaring the second condition
     {
-    Console.WriteLine("Введите число от 2 до 20");
-    return;
+    Console.WriteLine("Введите число от 2 до 20");  // Message output
+    return;   // Cycle completion
     }
     
-    result = numb % bas;
+    result = numb % bas;  // Formula for calculating the result
     
     
-    if (result < bas)
+    if (result < bas)   // Declaring the first settlement condition
       {
-      Console.WriteLine("Результат: " + result);
+      Console.WriteLine("Результат: " + result);  // Message output
       }
-    else 
+    else
     {
-      result = result % bas;
-      Console.WriteLine("Результат: " + result);
+      result = result % bas;  // Formula to match the condition
+      Console.WriteLine("Результат: " + result);  // Message output
     }
   }
 }

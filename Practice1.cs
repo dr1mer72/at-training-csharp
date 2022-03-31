92223396c5a406bc055a6e6de65697e61d4c7d04
dr1mer.vm <dr1mer.vm@gmail.com>
2022-03-31 16:49:51 +0500

    modify code

--- Practice1.cs
+++ Practice1.cs
@@ -3,7 +3,7 @@ using System;   // Namespace Usage
 class Practice1 {   // Class declaration
   static void Main() {  // Metod declaration
     
-    int result;   // Variable declaration
+    string result = "";   // Declaring a Variable for the Result
     
     Console.WriteLine("Введите число в десятичной системе: ");  // Message output
     int numb = Convert.ToInt32(Console.ReadLine());   // Variable declaration and type conversion
@@ -13,26 +13,25 @@ class Practice1 {   // Class declaration
     
     if (bas < 2)  // Declaring the first condition
     {
-    Console.WriteLine("Введите число от 2 до 20");  // Message output
+    Console.WriteLine("Ошибка: Введите число от 2 до 20");  // Message output
     return;   // Cycle completion
     }
     else if (bas > 20)  // Declaring the second condition
     {
-    Console.WriteLine("Введите число от 2 до 20");  // Message output
+    Console.WriteLine("Ошибка: Введите число от 2 до 20");  // Message output
     return;   // Cycle completion
     }
     
-    result = numb % bas;  // Formula for calculating the result
+    int temp = 0;   // Declaring a temporary variable
     
-    
-    if (result < bas)   // Declaring the first settlement condition
-      {
-      Console.WriteLine("Результат: " + result);  // Message output
-      }
-    else
-    {
-      result = result % bas;  // Formula to match the condition
-      Console.WriteLine("Результат: " + result);  // Message output
-    }
+    if (numb > 0)     // The condition that the number is greater than 0
+                while (numb >= (bas - 1))   // Loop until the number is greater than the base
+                {
+                    temp = numb % bas;      
+                    numb = (numb - temp) / bas;
+                    result = Convert.ToString(temp) + result;
+                }
+    result = Convert.ToString(numb) + result;
+    Console.WriteLine("Результат: " + result);  // Message output
   }
 }
